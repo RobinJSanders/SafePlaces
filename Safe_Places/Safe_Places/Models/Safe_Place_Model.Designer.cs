67d194a01f781f870835dbedb702d325dd1c41e5
RobinJSanders <robinjsanders@yahoo.co.uk>
2017-12-19 21:06:05 +0000

    Fixed a few minor visual errors on About page

--- Safe_Places/Safe_Places/Models/Safe_Place_Model.Designer.cs
+++ Safe_Places/Safe_Places/Models/Safe_Place_Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Brrucie\Desktop\Safe_Places_1.0\Safe_Places_1.0\Safe_Places\Models\Safe_Place_Model.edmx'. 
+﻿// T4 code generation is enabled for model 'J:\Yr3\enterprise\Implementation\safe_places_repos\Safe_Places\Safe_Places\Models\Safe_Place_Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
