9cab8bc9cd3c50b5e85060dcfa355dd4b18bac4d
RobinJSanders <robinjsanders@yahoo.co.uk>
2018-01-05 10:23:44 +0000

    Change Archive.cs
    Added display names for some of the archive/report  fields.

--- Safe_Places/Safe_Places/Models/Archive.cs
+++ Safe_Places/Safe_Places/Models/Archive.cs
@@ -11,16 +11,23 @@ namespace Safe_Places.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+
     public partial class Archive
     {
+        [DisplayName("Id")]
         public int Archive_ID { get; set; }
+        [DisplayName("Employee name ")]
+
         public string Employee_name { get; set; }
         public string Situation { get; set; }
+        [DisplayName("If other")]
         public string If_other { get; set; }
+        [DisplayName("How it was Resolved")]
         public string Resolved { get; set; }
         public string Critical { get; set; }
         public string Details { get; set; }
+        [DisplayName("Date")]
         public System.DateTime Archive_Date { get; set; }
     }
 }
